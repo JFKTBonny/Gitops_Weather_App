80d7c13fa747db8500435c3b2907a0266183fd25
JFKTBonny <jkamkotoyip@yahoo.com>
2025-08-23 20:57:49 +0200

    update gha workflows yaml

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,18 +4,18 @@ variable "environment" {
   default     = "real"
 }
 
-variable "location" {
-  description = "The location in which the resources will be created."
-  type        = string
-  default     = "switzerlandnorth"
+# variable "location" {
+#   description = "The location in which the resources will be created."
+#   type        = string
+#   default     = "switzerlandnorth"
 
-}
+# }
 
-variable "rg" {
-  description = "The name of the resource group in which to create the resources."
-  type        = string
-  default     = "gitops-real-rg"
-}
+# variable "rg" {
+#   description = "The name of the resource group in which to create the resources."
+#   type        = string
+#   default     = "gitops-real-rg"
+# }
 
 variable "region" {
   default = "us-east-1"
