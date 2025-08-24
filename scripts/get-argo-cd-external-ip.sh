18ea5699768ca6dd381357044ce388eabc65cdfc
JFKTBonny <jkamkotoyip@yahoo.com>
2025-08-24 11:56:19 +0200

    added Argo CD server deployment  steps

--- scripts/get-argo-cd-external-ip.sh
+++ scripts/get-argo-cd-external-ip.sh
@@ -10,7 +10,7 @@ export EXTERNAL_IP=""
 # Loop until the external IP is assigned
 while [ -z $EXTERNAL_IP ]; do
     echo "Waiting for external IP..."
-    EXTERNAL_IP=$(kubectl get svc $SERVICE_NAME -n $NAMESPACE - | tail -2 | head -1 | awk '{print $4}')
+    EXTERNAL_IP=$(kubectl get svc argocd-server -n argocd - | tail -2 | head -1 | awk '{print $4}')
     # Wait for 10 seconds before checking again
     sleep 10
 done
