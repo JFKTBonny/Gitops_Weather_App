ea63c59df92c0395ca5db0543479321982ce7b90
JFKTBonny <jkamkotoyip@yahoo.com>
2025-08-24 12:16:49 +0200

    added Argo CD server deployment  steps

--- scripts/get-argo-cd-external-ip.sh
+++ scripts/get-argo-cd-external-ip.sh
@@ -4,13 +4,13 @@
 SERVICE_NAME="argocd-server"
 NAMESPACE="argocd"
 
-# # Initialize external IP
-export EXTERNAL_IP=""
+# Initialize external IP
+EXTERNAL_IP=""
 
 # Loop until the external IP is assigned
-while [ -z $EXTERNAL_IP ]; do
+while [ -z "$EXTERNAL_IP" ]; do
     echo "Waiting for external IP..."
-    EXTERNAL_IP=$(kubectl get service argocd-server -n argocd - | tail -2 | head -1 | awk '{print $4}')
+    EXTERNAL_IP=$(kubectl get service "$SERVICE_NAME" -n "$NAMESPACE" | tail -2 | head -1 | awk '{print $4}')
     # Wait for 10 seconds before checking again
     sleep 10
 done
--- scripts/get-argo-cd-initial-password.sh
+++ scripts/get-argo-cd-initial-password.sh
@@ -6,18 +6,18 @@ NAMESPACE="argocd"
 # Define the name of the secret where the initial password is stored
 SECRET_NAME="argocd-initial-admin-secret"
 
-export ARGO_CD_PASSWORD=""
-
+# Initialize ARGO_CD_PASSWORD variable
+ARGO_CD_PASSWORD=""
 
 # Function to get the Argo CD admin password
 function get_argo_cd_password() {
     # Loop until the password is retrieved
     while true; do
         # Extract the password from the secret
-        ARGO_CD_PASSWORD=$(kubectl get secret $SECRET_NAME -n $NAMESPACE -o jsonpath="{.data.password}" 2>/dev/null | base64 --decode)
+        ARGO_CD_PASSWORD=$(kubectl get secret "$SECRET_NAME" -n "$NAMESPACE" -o jsonpath="{.data.password}" 2>/dev/null | base64 --decode)
         
-        # Check if password is successfully retrieved and non-empty
-        if [[ ! -z "$ARGO_CD_PASSWORD" ]]; then
+        # Check if the password is successfully retrieved and non-empty
+        if [[ -n "$ARGO_CD_PASSWORD" ]]; then
             echo "Successfully retrieved the initial password for Argo CD."
             echo "Initial Admin Password: $ARGO_CD_PASSWORD"
             break  # Exit the loop if password is retrieved
@@ -29,7 +29,7 @@ function get_argo_cd_password() {
 }
 
 # Check if the secret exists in the specified namespace
-if kubectl get secret $SECRET_NAME -n $NAMESPACE &> /dev/null; then
+if kubectl get secret "$SECRET_NAME" -n "$NAMESPACE" &> /dev/null; then
     echo "Secret '$SECRET_NAME' found in namespace '$NAMESPACE'."
     get_argo_cd_password
 else
