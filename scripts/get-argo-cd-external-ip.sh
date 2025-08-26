e375d16350e3ccfd4d99b2cd93d088e617b81b30
JFKTBonny <jkamkotoyip@yahoo.com>
2025-08-26 20:40:27 +0200

    update

--- scripts/get-argo-cd-external-ip.sh
+++ scripts/get-argo-cd-external-ip.sh
@@ -10,7 +10,8 @@ EXTERNAL_IP=""
 # Loop until the external IP is assigned
 while [ -z "$EXTERNAL_IP" ]; do
     echo "Waiting for external IP..."
-    EXTERNAL_IP=$(kubectl get service "$SERVICE_NAME" -n "$NAMESPACE" | tail -2 | head -1 | awk '{print $4}')
+    EXTERNAL_IP=$(kubectl get service "$SERVICE_NAME" -n "$NAMESPACE" -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
+
     # Wait for 10 seconds before checking again
     sleep 10
 done
